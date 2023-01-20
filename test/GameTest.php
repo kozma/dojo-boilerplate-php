68839cb19a08343530b0fb8526f92bb87ee389fd
Peter Kozma <peter.kozma@emarsys.com>
2023-01-20 13:05:20 +0100

    original files

---
+++ test/GameTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class GameTest extends TestCase
+{
+
+    /** @test */
+    public function hello(): void
+    {
+        $this->assertEquals(44,6);
+    }
+}
